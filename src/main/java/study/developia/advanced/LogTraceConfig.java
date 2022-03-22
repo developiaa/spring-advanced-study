8592aaf1dd01ece849925251312796cd18367973
developiaa <developiaa@gmail.com>
2022-03-22 21:20:37 +0900

    feat : Add threadlocal component

--- src/main/java/study/developia/advanced/LogTraceConfig.java
+++ src/main/java/study/developia/advanced/LogTraceConfig.java
@@ -2,14 +2,14 @@ package study.developia.advanced;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import study.developia.advanced.trace.newlogtrace.FieldLogTrace;
 import study.developia.advanced.trace.newlogtrace.LogTrace;
+import study.developia.advanced.trace.newlogtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
